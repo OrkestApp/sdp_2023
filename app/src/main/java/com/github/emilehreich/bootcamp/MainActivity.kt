8a9d1049419db68362783fc7ac5f6bcf2c24ed77
evguersl <evgueni.rousselot@gmail.com>
2023-03-01 15:23:11 +0100

    v1 works

--- app/src/main/java/com/github/emilehreich/bootcamp/MainActivity.kt
+++ app/src/main/java/com/github/emilehreich/bootcamp/MainActivity.kt
@@ -4,12 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnSignOut: Button
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        btnSignOut = findViewById(R.id.btn_sign_out)
+        btnSignOut.setOnClickListener {
+            signOut()
+        }
     }
 
     fun greetName(view: View) {
@@ -17,7 +31,14 @@ class MainActivity : AppCompatActivity() {
         val name = findViewById<TextView>(R.id.mainText)
         intent.putExtra("name",name.text.toString())
         startActivity(intent)
+    }
 
-
+    private fun signOut() {
+        auth.signOut()
+        val intent = Intent(this@MainActivity, SignIn::class.java)
+        FirebaseAuth.getInstance().signOut()
+        GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_SIGN_IN).revokeAccess()
+        startActivity(intent)
+        finish()
     }
 }

966a1f5b5d0bf54a52c3273b9f052f558010d25c
Emile Hreich <emile.jd.hreich@gmail.com>
2023-03-02 11:16:22 +0100

    setup Database emulator for testing

--- app/src/androidTest/java/com/github/emilehreich/bootcamp/MainActivityTest.kt
+++ app/src/androidTest/java/com/github/emilehreich/bootcamp/MainActivityTest.kt
@@ -1,21 +1,20 @@
 package com.github.emilehreich.bootcamp
 
 import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers.allOf
+import com.github.emilehreich.bootcamp.MainActivity.Companion.database
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.regex.Pattern.matches
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -23,29 +22,23 @@ class MainActivityTest {
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
-//    @Test
-//    fun testWritingOfName() {
-//        onView(ViewMatchers.withId(R.id.mainText)).perform(ViewActions.replaceText("Steve"))
-//        closeSoftKeyboard()
-//        onView(withText("Steve")).check(matches(isDisplayed()))
-//    }
-//
-//    @Test
-//    fun testStartButton() {
-//        Intents.init()
-//
-//        onView(withId(R.id.mainText)).perform(ViewActions.replaceText("Steve"))
-//        closeSoftKeyboard()
-//
-//        onView(withId(R.id.setButton)).perform(click())
-//        intended(
-//            allOf(
-//                hasComponent(GreetingActivity::class.java.name),
-//                hasExtra("name", "Steve")
-//            )
-//        )
-//
-//        Intents.release()
-//    }
+    @Test
+    fun setAndGet() {
+        // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+        // the host computer from an Android emulator.
+        database = Firebase.database
+        database.useEmulator("10.0.2.2", 9000)
+
+        onView(withId(R.id.phoneBox)).perform(typeText("1234567890"))
+        onView(withId(R.id.mailBox)).perform(typeText("test@wouhou.ch"))
+
+        onView(withId(R.id.setButton)).perform(click())
+
+        onView(withId(R.id.mailBox)).perform(typeText(""))
+        onView(withId(R.id.getButon)).perform(click())
+
+        // check if displayed mail is correct
+        onView(withText("test@wouhou.ch")).check(matches(isDisplayed()))
+    }
 
 }
--- app/src/main/java/com/github/emilehreich/bootcamp/MainActivity.kt
+++ app/src/main/java/com/github/emilehreich/bootcamp/MainActivity.kt
@@ -3,36 +3,53 @@ package com.github.emilehreich.bootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.DisplayNameSources.EMAIL
 import android.view.View
 import android.widget.TextView
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.concurrent.CompletableFuture
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        var database = Firebase.database
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    // getButton Clicked
+    // on click getButton the email address associated with the phone number
     fun get(view: View) {
         // get the email address from the mail field
         val mail = findViewById<TextView>(R.id.mailBox)
         val phone = findViewById<TextView>(R.id.phoneBox)
 
+        val future = CompletableFuture<String>()
         val db: DatabaseReference = Firebase.database.reference
-//        db.child(PHONE_NB).setValue(EMAIL)
+
+        db.child(phone.text.toString()).get().addOnSuccessListener {
+            if (it.value == null) future.completeExceptionally(NoSuchFieldException())
+            else future.complete(it.value as String)
+        }.addOnFailureListener {
+            future.completeExceptionally(it)
+        }
+        future.thenAccept {
+            mail.text = it
+        }
     }
 
     // set phone and email
     fun set(view: View) {
+        val db = database.reference
         // get the email address from the mail field
         val mail = findViewById<TextView>(R.id.mailBox)
         val phone = findViewById<TextView>(R.id.phoneBox)
-        // todo
+
+        db.child(phone.text.toString()).setValue(mail.text.toString())
     }
 
 }

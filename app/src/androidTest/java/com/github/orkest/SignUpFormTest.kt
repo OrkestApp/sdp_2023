47850710efca4190a8e7fda12781c6c2f4d74b7b
yasmineben510 <75130336+yasmineben510@users.noreply.github.com>
2023-03-11 14:51:05 +0100

    Update SignUpFormTest.kt

--- app/src/androidTest/java/com/github/orkest/SignUpFormTest.kt
+++ app/src/androidTest/java/com/github/orkest/SignUpFormTest.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.navigation.compose.rememberNavController
 import com.github.orkest.Model.Providers
 import com.github.orkest.View.auth.SignUpForm
+import com.github.orkest.ViewModel.auth.AuthViewModel
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test

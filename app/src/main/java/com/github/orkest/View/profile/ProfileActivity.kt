a54ba6caf7868fe332d4781d4e86d37ad64ff934
Roman <r.danylovych@gmail.com>
2023-03-26 16:10:57 +0200

    add navigation drawer in profile activity

--- app/src/main/java/com/github/orkest/View/profile/ProfileActivity.kt
+++ app/src/main/java/com/github/orkest/View/profile/ProfileActivity.kt
@@ -3,11 +3,20 @@ package com.github.orkest.View.profile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.orkest.View.*
 import com.github.orkest.ViewModel.profile.ProfileViewModel
 import com.github.orkest.ui.theme.OrkestTheme
 
@@ -23,7 +32,7 @@ class ProfileActivity() : ComponentActivity() {
         val viewModel = ProfileViewModel(currentUser)
         setContent {
             ProfileActivitySetting {
-                ProfileActivityScreen(viewModel = viewModel)
+                ProfileActivityScreen(this, viewModel = viewModel)
             }
         }
     }
@@ -43,16 +52,48 @@ fun ProfileActivitySetting(content: @Composable () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileActivityScreen(viewModel: ProfileViewModel) {
-    Column() {
-        topProfile(viewModel = viewModel)
-        Divider()
-        favoriteSongs()
-        Text(text="AHHHHHHHHHHHH")
-        Divider()
-        favoriteArtists()
-    }
+fun ProfileActivityScreen(activity: ComponentActivity, viewModel: ProfileViewModel) {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+
+    androidx.compose.material.Scaffold(
+        // keep track of the state of the scaffold (whether it is opened or closed)
+        scaffoldState = scaffoldState,
+        topBar = {
+            TopAppBar(
+                title = {
+                },
+                navigationIcon = {
+                    NavDrawerButton(coroutineScope, scaffoldState)
+                }
+            )
+        },
+        // The content displayed inside the drawer when you click on the hamburger menu button
+        drawerContent = { CreateMenuDrawer() },
+
+        content = { padding ->
+            Modifier
+                .fillMaxHeight()
+                .padding(padding)
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(8.dp),
+            ) {
+                topProfile(viewModel = viewModel)
+                Divider()
+                favoriteSongs()
+                Divider()
+                favoriteArtists()
+
+            }
+        },
+        drawerGesturesEnabled = true
+    )
+
 }
 
 @Composable
@@ -62,7 +103,17 @@ fun topProfile(viewModel: ProfileViewModel) {
 
 @Composable
 fun favoriteSongs() {
-    
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text("Favorite Songs")
+        IconButton(onClick = { }) {
+            Icon(imageVector = Icons.Outlined.Add, contentDescription = "Add Button")
+        }
+    }
 }
 
 @Composable
@@ -76,6 +127,6 @@ fun favoriteArtists() {
 @Composable
 fun DefaultProfilePreview() {
     ProfileActivitySetting {
-        ProfileActivityScreen(viewModel = ProfileViewModel("JohnDoe"))
+        ProfileActivityScreen(ProfileActivity(), viewModel = ProfileViewModel("JohnDoe"))
     }
 }

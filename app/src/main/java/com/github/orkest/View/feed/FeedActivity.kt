1b53410e1ca071afc617fe57c888750849d2e730
yasmineben510 <75130336+yasmineben510@users.noreply.github.com>
2023-03-27 16:59:48 +0200

    added the reaction buttons to the post

--- app/src/main/java/com/github/orkest/View/feed/FeedActivity.kt
+++ app/src/main/java/com/github/orkest/View/feed/FeedActivity.kt
@@ -12,8 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,57 +27,67 @@ import com.github.orkest.R
 
 @Composable
 fun FeedActivity(){
-
     //Add the post
     Post()
-
 
 }
 
 @Composable
 fun Post(){
 
-    Row(modifier = Modifier.padding(20.dp)){
-
-        // Display the user profile pic
-        UserProfilePic()
-
-        Spacer(modifier = Modifier.width(10.dp))
-
-        Box(Modifier
-            .clip(shape = RoundedCornerShape(20.dp))
-            .background(Color.hsl(60f, 0.16f, 0.92f))
-            .padding(10.dp, top= 10.dp, end= 10.dp)) {
+    Row(modifier = Modifier
+        .padding(20.dp)
+        .clip(shape = RoundedCornerShape(20.dp))
+        //.background(Color.hsl(60f, 0.16f, 0.92f))
+        //.background(Color.hsl(180f, 0.11f, 0.98f))
+        .background(Color.DarkGray)){
 
 
-            Column {
-                //Add the user's username
-                Text(text = "Username", fontSize = 17.sp)
+        Column {
 
-                Spacer(modifier = Modifier.height(10.dp))
+            // Display the user profile pic
+            UserProfilePic()
 
-                // Display the post's content
-                PostDescription()
-                Spacer(modifier = Modifier.height(10.dp))
 
-                // Display the post's song
-                SongCard()
-                Spacer(modifier = Modifier.height(10.dp))
+            //Display the reaction buttons
+            Reaction()
+        }
 
-            }
+
+
+        Column(modifier = Modifier.padding(10.dp, top = 10.dp, end = 10.dp)) {
+            //Add the user's username
+            UserUsername()
+
+            // Display the post's content
+            PostDescription()
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Display the post's song
+            SongCard()
+            Spacer(modifier = Modifier.height(10.dp))
         }
     }
-    
+}
+
+@Composable
+fun UserUsername(){
+    //Add the user's username
+    Text(text = "Username", fontSize = 14.sp, fontWeight = FontWeight.Bold,
+        color = Color.White,
+        modifier = Modifier
+          //  .padding(start = 10.dp)
+            .clickable {  })
 }
 
 @Composable
 fun UserProfilePic(){
-
         //Add the user's profile pic
         Image(
             painter = painterResource(id = R.drawable.profile_picture),
             contentDescription = "Profile Picture of the user",
             modifier = Modifier
+                .padding(start = 10.dp, top = 10.dp)
                 .height(40.dp)
                 .width(40.dp)
                 .clip(shape = CircleShape)
@@ -82,11 +97,11 @@ fun UserProfilePic(){
 
 @Composable
 fun PostDescription(){
-    Box() {
         //Add the post's content
-        Text(text = "Post Content", fontSize = 18.sp)
-    }
-
+        Text(text = "Post Content",
+            fontSize = 15.sp,
+            color = Color.White,
+            maxLines = 2)
 }
 
 @Composable
@@ -99,12 +114,12 @@ fun SongCard(){
                 .wrapContentSize()
                 .clip(shape = RoundedCornerShape(20.dp))
                 .background(Color.hsl(54f, 1f, 0.5f))
-                .padding(end= 10.dp)){
+                .padding(end = 10.dp)){
 
             //Add the song's info at the left of the card
             SongInfo()
 
-            Spacer(modifier = Modifier.width(20.dp))
+            Spacer(modifier = Modifier.width(5.dp))
 
             //Add a play button at the right of the card
             PlayButton()
@@ -149,9 +164,55 @@ fun PlayButton(){
             .clickable { })
 }
 
+@Composable
+fun Reaction(){
+    Column(modifier = Modifier.padding(20.dp)) {
+        // Create the like button
+        Icon(painter = painterResource(id = R.drawable.black_like_icon),
+            contentDescription = "Like button",
+            tint = Color.White,
+            modifier = Modifier
+                .testTag("like_button")
+                .height(20.dp)
+                .width(20.dp)
+                .clickable { })
+       //Text(text = "20", color = Color.White, fontSize = 9.sp)
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        //Create the comment button
+        Icon(painter = painterResource(id = R.drawable.comment_icon),
+            contentDescription = "Comment button",
+            tint = Color.White,
+            modifier = Modifier
+                .testTag("comment_button")
+                .height(20.dp)
+                .width(20.dp)
+                .clickable { })
+//       Text(text = "10", color = Color.White, fontSize = 9.sp,
+//       textAlign = TextAlign.Center)
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        //Create the share button
+        Icon(painter = painterResource(id = R.drawable.share_icon),
+            contentDescription = "Share button",
+            tint = Color.White,
+            modifier = Modifier
+                .testTag("share_button")
+                .height(20.dp)
+                .width(20.dp)
+                .clickable { })
+       // Text(text = "5", color = Color.White, fontSize = 9.sp)
+
+    }
+
+}
+
 
 @Preview
 @Composable
 fun PreviewSongCard(){
-    Post()
+Post()
+
 }

574a716b49be9bfa59953298025625653b314325
Roman <r.danylovych@gmail.com>
2023-03-29 23:51:18 +0200

    add documentation and a publish button

---
+++ app/src/main/java/com/github/orkest/Model/Comment.kt
@@ -0,0 +1,13 @@
+package com.github.orkest.Model
+
+/**
+ * Class defining comments users are able to publish under other people's posts
+ * @param username: username of the user publishing the comment
+ * @param text: the actual content of the comment
+ * @param postId: id of the post under which the comment is published
+ */
+data class Comment(
+    val username: String,
+    val text: String,
+    val postId: Int
+    )
--- app/src/main/java/com/github/orkest/View/publications/CommentActivity.kt
+++ app/src/main/java/com/github/orkest/View/publications/CommentActivity.kt
@@ -10,6 +10,9 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Android
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.*
@@ -18,9 +21,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
+import com.github.orkest.Model.Comment
 import com.github.orkest.R
 import com.github.orkest.View.theme.OrkestTheme
 
@@ -49,69 +54,104 @@ fun CommentSetting(content: @Composable () -> Unit) {
     }
 }
 
+/**
+ * Main screen on which this the Comment activity is built
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CommentScreen(post: ComponentActivity) {
+fun CommentScreen(post: ComponentActivity) { // TODO must become FeedActivity to use Post() Composable
+    val comments = (1..15).map { Comment(username = "roman n° ${it}", text = "oh nah", postId = it)}
     Column() {
-        Surface(modifier = Modifier.height(600.dp)) {
-            Comments()
+        // The Modifier.weight(1f, false) is to have a sticky footer (which is the TextField)
+        Surface(modifier = Modifier.weight(1f, false)) {
+            Comments(comments)
         }
-        var modifyName by rememberSaveable { mutableStateOf("tqt") }
+        Divider()
+        var modifyName by rememberSaveable { mutableStateOf("") }
         TextField(
+            modifier = Modifier.fillMaxWidth(),
             value = modifyName,
             onValueChange = { modifyName = it },
             colors = TextFieldDefaults.textFieldColors(
-                containerColor = Color.Transparent
-            )
+                containerColor = Color.Transparent,
+            ),
+            leadingIcon = { displayProfilePic() },//Icon(imageVector = Icons.Default.Android, contentDescription = "User Profile Pic") },
+            trailingIcon = { PublishButton() },
+            //label = { Text(text = "Username") },
+            placeholder = { Text(text = "Write your thoughts...") }
         )
     }
 
 }
 
+/**
+ * Displays the comments under a post
+ * @param comments: the list of comments to be displayed
+ */
 @Composable
-fun Comments() {
-    val comments = (1..30).map { "Item $it" }
+fun Comments(comments: List<Comment>) {
     LazyColumn {
-        itemsIndexed(comments) { index, comment ->
+        itemsIndexed(comments) { _, comment ->
             Divider()
-            CommentBox(
-                profilePic = "",
-                username = "Roman ${index}",
-                comment = comment
-            )
+            CommentBox(comment = comment)
         }
     }
 }
 
+/**
+ * Comment design
+ * @param comment: the comment to display
+ */
 @Composable
-fun CommentBox(profilePic: String, username: String, comment: String) {
+fun CommentBox(comment: Comment) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
     ) {
         Row {
             displayProfilePic()
-            Column() {
-                Text(text = username, fontWeight = FontWeight.Bold)
-                Text(text = comment)
+            Column {
+                Text(text = comment.username, fontWeight = FontWeight.Bold)
+                Text(text = comment.text)
             }
         }
     }
 }
 
+/**
+ * standardized 'displayer' of profile pics on comments
+ * TODO: need to decide on how to reference people's profile pictures
+ */
 @Composable
-fun displayProfilePic() {
+fun displayProfilePic(/* TODO */) {
     Card(
         shape = CircleShape,
         modifier = Modifier
             .padding(8.dp)
-            .size(50.dp)
+            .size(40.dp)
     ) {
         val imageUri = rememberSaveable { mutableStateOf("") }
         val painter = rememberImagePainter(
             imageUri.value.ifEmpty { R.drawable.blank_profile_pic }
         )
-        Image(painter = painter, contentDescription = "tqt")
+        Image(painter = painter, contentDescription = "Profile Picture")
+    }
+}
+
+
+/**
+ * "Send" button that when pressed will publish the typed comment
+ * TODO: add the onClick functionality
+ */
+@Composable
+fun PublishButton() {
+    IconButton(
+        onClick = { /* TODO */ }
+    ) {
+        Icon(
+            Icons.Default.Send,
+            contentDescription = "Send Button"
+        )
     }
 }
 

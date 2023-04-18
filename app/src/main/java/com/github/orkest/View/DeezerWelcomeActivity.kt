79a4dac19bd9f585bc3950e2febb31b9213bb23e
Jean-Francois/Zermelo <jean-francois.rocher@epfl.ch>
2023-04-18 02:09:24 +0200

    add the app link to redirect to our app, with deezer

--- app/src/main/java/com/github/orkest/View/DeezerWelcomeActivity.kt
+++ app/src/main/java/com/github/orkest/View/DeezerWelcomeActivity.kt
@@ -1,9 +1,11 @@
 package com.github.orkest.View
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material3.Button
@@ -11,8 +13,21 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 
 class DeezerWelcomeActivity : AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent: Intent = intent
+        val deepLink: String = intent.getData().toString()
+
+// Parse the deep link into a Uri object
+
+// Parse the deep link into a Uri object
+        val uri: Uri = Uri.parse(deepLink)
+
+// Get the value of a query parameter
+
+// Get the value of a query parameter
+        val param1Value: String? = uri.getQueryParameter("param1")
 
         setContent {
             createViewForDeezer()
@@ -23,8 +38,11 @@ class DeezerWelcomeActivity : AppCompatActivity(){
     @Composable
     fun createViewForDeezer(){
         var text by remember { mutableStateOf("Enter userName ") }
-        TextField(value = text, onValueChange = { text = it})
-        Button(onClick = { saveTokenInDatabase(); launchMainActivity()}, content = { Text("Start to use Deezer")})
+        Column() {
+            TextField(value = text, onValueChange = { text = it})
+            Button(onClick = { saveTokenInDatabase(); launchMainActivity()}, content = { Text("Start to use Deezer")})
+
+        }
 
 
     }

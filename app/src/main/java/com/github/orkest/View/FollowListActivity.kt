a2ac14a2359263aa2c9cc0ae785eee1b2aa43beb
saraanej <sara.anejjar@epfl.ch>
2023-04-19 12:15:53 +0200

    adding Constants

--- app/src/main/java/com/github/orkest/View/FollowListActivity.kt
+++ app/src/main/java/com/github/orkest/View/FollowListActivity.kt
@@ -38,6 +38,14 @@ import com.github.orkest.R
 //Here for preview purposes. Will be deleted.
 val user = User(username = "Philippe", profile = Profile(profilePictureId = R.drawable.blank_profile_pic))
 
+private val paddingValue = 5.dp
+private val titleFontSize = 28.sp
+private val textFontSize = 18.sp
+private val smallSeparator = 10.dp
+private val largeSeparator = 20.dp
+private val itemSize = 50.dp
+private val roundedCornerValue = 20.dp
+
 /**
  * This activity is launched when a user wants to display the list of followers or followings of another user (or himself)
  * Two extras are sent with the intent to launch the activity
@@ -76,24 +84,24 @@ fun FollowList(viewModel: FollowListViewModel){
                     painterResource(id = R.drawable.back_button),
                     contentDescription = "Back button",
                     modifier = Modifier
-                        .padding(5.dp)
+                        .padding(paddingValue)
                         .size(120.dp)
                 )
             }
-            Spacer(modifier = Modifier.width(20.dp))
+            Spacer(modifier = Modifier.width(largeSeparator))
 
             Column(
                 Modifier.fillMaxWidth(),
                 verticalArrangement = Arrangement.Center){
                 Text(
                     text = if (viewModel.isFollowersList) "Followers" else "Followings",
-                    style = TextStyle(fontSize = 28.sp),
-                    modifier = Modifier.padding(5.dp)
+                    style = TextStyle(fontSize = titleFontSize),
+                    modifier = Modifier.padding(paddingValue)
                 )
             }
         }
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(smallSeparator))
 
         //Here only to show the feature. Will be deleted.
         repeat(3){
@@ -106,30 +114,30 @@ fun FollowList(viewModel: FollowListViewModel){
 @Composable
 fun CreateProfilePreview(user: User){
     Row(modifier = Modifier
-        .padding(all = 5.dp)
+        .padding(all = paddingValue)
         .clickable {}
         .fillMaxWidth()
-        .clip(shape = RoundedCornerShape(20.dp))
+        .clip(shape = RoundedCornerShape(roundedCornerValue))
         .background(Color.DarkGray)
     ) {
         Image(
             painter = painterResource(user.profile.profilePictureId),
             contentDescription = "Contact profile picture",
             modifier = Modifier
-                .size(50.dp)
+                .size(itemSize)
                 .clip(CircleShape)
                 .padding(start = 10.dp)
         )
 
-        Spacer(modifier = Modifier.width(10.dp))
+        Spacer(modifier = Modifier.width(smallSeparator))
 
         Column (
-            modifier = Modifier.height(50.dp),
+            modifier = Modifier.height(itemSize),
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Center) {
             Text(
                 text = user.username,
-                style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                style = TextStyle(fontSize = textFontSize, fontWeight = FontWeight.Bold)
             )
         }
     }

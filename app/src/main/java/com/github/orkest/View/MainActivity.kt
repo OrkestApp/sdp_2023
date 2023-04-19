12e6eed9d7f9b5d9c6d5a1d7f5f69c1b5c5cacc8
evguersl <evgueni.rousselot@gmail.com>
2023-04-19 18:39:10 +0200

    message to enable notifications & sign out example works

--- app/src/main/java/com/github/orkest/View/MainActivity.kt
+++ app/src/main/java/com/github/orkest/View/MainActivity.kt
@@ -2,8 +2,18 @@ package com.github.orkest.View
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.compose.rememberNavController
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.provider.Settings
+import androidx.core.app.NotificationManagerCompat
+import android.graphics.Color
+
+private const val NOTIFICATION_CHANNEL_ID = "your_channel_id"
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +27,61 @@ class MainActivity : AppCompatActivity() {
             }
             NavigationBar.CreateNavigationBar(navController = rememberNavController(), user)
         }
+
+        createNotificationChannel(this)
+        promptUserToEnableNotifications(this)
     }
+
+    /**
+     * The notification channel is required for Android 8.0 and above
+     * To inform the user to enable the notifications for this app
+     */
+    fun createNotificationChannel(context: Context) {
+        val name = "Your Channel Name"
+        val descriptionText = "Your Channel Description"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    /**
+     * Build the notification of enabling them
+     */
+    fun promptUserToEnableNotifications(activity: Context) {
+        val notificationManager = NotificationManagerCompat.from(activity)
+        if (!notificationManager.areNotificationsEnabled()) {
+            val alertDialog = AlertDialog.Builder(activity)
+                .setTitle("Enable Notifications")
+                .setMessage("To receive important updates, please enable notifications for this app.")
+                .setPositiveButton("Enable", null)
+                .setNegativeButton("Cancel", null)
+                .create()
+
+            alertDialog.setOnShowListener {
+                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).apply {
+                    setTextColor(Color.BLUE)
+                    setOnClickListener {
+                        val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                            putExtra(Settings.EXTRA_APP_PACKAGE, activity.packageName)
+                        }
+                        activity.startActivity(intent)
+                        alertDialog.dismiss()
+                    }
+                }
+                alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).apply {
+                    setTextColor(Color.RED)
+                }
+            }
+
+            alertDialog.show()
+        } else {
+            // Notifications are already enabled
+        }
+    }
+
 }
+
+
--- app/src/main/java/com/github/orkest/View/profile/ProfileTopInterface.kt
+++ app/src/main/java/com/github/orkest/View/profile/ProfileTopInterface.kt
@@ -1,9 +1,11 @@
 package com.github.orkest.View.profile
 
+import android.content.Context
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -11,9 +13,7 @@ import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.*
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,16 +26,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
-import androidx.navigation.NavController
-import com.github.orkest.Model.Profile
 import com.github.orkest.R
 import com.github.orkest.View.EditProfileActivity
-import com.github.orkest.View.MainActivity
 import com.github.orkest.ViewModel.profile.ProfileViewModel
 import androidx.compose.ui.graphics.Color
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat.getSystemService
 import com.github.orkest.View.auth.AuthActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -99,31 +94,7 @@ fun ProfileTopInterface(viewModel: ProfileViewModel) {
                 auth.signOut()
 
                 //notification
-
-                // Create an explicit intent for an Activity in your app
-                val intent = Intent(this, AlertDetails::class.java).apply {
-                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                }
-                val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-                val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setSmallIcon(R.drawable.notification_icon)
-                    .setContentTitle("My notification")
-                    .setContentText("Hello World!")
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                    // Set the intent that will fire when the user taps the notification
-                    .setContentIntent(pendingIntent)
-                    .setAutoCancel(true)
-
-                with(NotificationManagerCompat.from(this)) {
-                    // notificationId is a unique int for each notification that you must define
-                    notify(notificationId, builder.build())
-                }
-
-
-
-                //
-
+                sendNotification(context, "Orkest", "You've signed out ;)", "channel_id_signout")
 
                 //uncomment if un-caching is needed
                 GoogleSignIn.getClient(context, GoogleSignInOptions.DEFAULT_SIGN_IN).signOut()
@@ -133,21 +104,24 @@ fun ProfileTopInterface(viewModel: ProfileViewModel) {
     }
 }
 
-private fun createNotificationChannel() {
-    // Create the NotificationChannel, but only on API 26+ because
-    // the NotificationChannel class is new and not in the support library
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        val name = getString(R.string.channel_name)
-        val descriptionText = getString(R.string.channel_description)
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-            description = descriptionText
-        }
-        // Register the channel with the system
-        val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-    }
+/**
+ * Send a Notification for the user when signing out
+ * This is an example to later finish developing the notifications
+ */
+fun sendNotification(context: Context, title: String, message: String, channelId: String) {
+    val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    val channel = NotificationChannel(channelId, "Sign Out Channel", NotificationManager.IMPORTANCE_DEFAULT)
+    notificationManager.createNotificationChannel(channel)
+
+    val notificationBuilder = NotificationCompat.Builder(context, channelId)
+        .setContentTitle(title)
+        .setContentText(message)
+        .setSmallIcon(android.R.drawable.ic_dialog_info)
+
+    notificationManager.notify(0, notificationBuilder.build())
+
+    Log.d("Notification",title)
 }
 
 

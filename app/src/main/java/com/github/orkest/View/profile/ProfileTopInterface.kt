eed1ee6ab6b97130db0b835e9d537c6e50e4f27a
Emile Hreich <emile.janhodithreich@epfl.ch>
2023-04-21 00:12:29 +0200

    fix ambiguous imports

--- app/src/main/java/com/github/orkest/View/profile/ProfileTopInterface.kt
+++ app/src/main/java/com/github/orkest/View/profile/ProfileTopInterface.kt
@@ -41,7 +41,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 import java.util.concurrent.CompletableFuture
-import kotlinx.coroutines.CoroutineScope
 
 
 

15a95759f7b3af074ab852540b6a22a76c3caec0
yasmineben510 <75130336+yasmineben510@users.noreply.github.com>
2023-04-30 21:07:57 +0200

    Modularized shazamSong

--- app/src/main/java/com/github/orkest/shazam/ui/ShazamActivity.kt
+++ app/src/main/java/com/github/orkest/shazam/ui/ShazamActivity.kt
@@ -2,12 +2,14 @@ package com.github.orkest.shazam.ui
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.platform.LocalContext
+import com.github.orkest.data.Song
 import com.github.orkest.shazam.data.AudioRecognition
 import com.github.orkest.shazam.data.AudioRecording
 import com.github.orkest.shazam.domain.ShazamConstants
@@ -31,35 +33,47 @@ import kotlinx.coroutines.launch
                 if (!permission)
                     ShazamConstants.askRecordPermission(activity)
                 else {
-                    //Starts the recording and the recognition
-                    Toast.makeText(context, "Shazaming ...",
-                        Toast.LENGTH_LONG).show()
+                    Toast.makeText(context, "Shazaming ...", Toast.LENGTH_LONG).show()
 
-                    val title = AudioRecognition.recognizeSong(
-                        coroutineScope, activity,
+                    //Starts the recording and the recognition
+                    val title = AudioRecognition.recognizeSong(coroutineScope, activity,
                         AudioRecording.recordingFlow(coroutineScope)
                     )
 
                     //Handles the result of the recognition
                     title.whenComplete { song, error ->
-                        if (error != null) {
-                            Log.d("ShazamActivity", "Error : $error")
-                        } else {
-                            //TODO: Send the song to the posting UI when found
+                        handleRecognitionResult(song, error, context) }
 
-                            Log.d("ShazamActivity", "Song : $song")
-                            //NO MATCH WAS MADE
-                            if (song == ShazamConstants.SONG_NO_MATCH) {
-                                Toast.makeText(context, "No match found for this song",
-                                    Toast.LENGTH_LONG).show()
-                            } else {
-                                //MATCH WAS MADE
-                                Toast.makeText(context, "Song found : ${song.Title} by ${song.Artist}",
-                                    Toast.LENGTH_LONG).show()
-                            }
-                        }
-                    }
                 }
+            }
+        }
+    }
+
+    /**
+     * Handles the result of the recognition
+     * Displays different toasts depending on the result
+     * @param song the song found
+     * @param error the error
+     * @param context the context
+     */
+    private fun handleRecognitionResult(song: Song, error: Throwable?, context: Context) {
+
+        if (error != null) {
+            Log.d("ShazamActivity", "Error : $error")
+        } else {
+            Log.d("ShazamActivity", "Song : $song")
+            //NO MATCH WAS MADE
+            if (song == ShazamConstants.SONG_NO_MATCH) {
+                Toast.makeText(
+                    context, "No match found for this song",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                //MATCH WAS MADE
+                Toast.makeText(
+                    context, "Song found : ${song.Title} by ${song.Artist}",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }

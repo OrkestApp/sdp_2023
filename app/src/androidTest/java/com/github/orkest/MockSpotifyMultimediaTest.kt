c338b8b975741f18b9e53cbed04dd0758e8d1b64
Emile Hreich <emile.janhodithreich@epfl.ch>
2023-05-18 18:10:33 +0200

    Update MockSpotifyMultimediaTest.kt

--- app/src/androidTest/java/com/github/orkest/MockSpotifyMultimediaTest.kt
+++ app/src/androidTest/java/com/github/orkest/MockSpotifyMultimediaTest.kt
@@ -1,45 +1,36 @@
 package com.github.orkest
-
 import com.github.orkest.data.SpotifyMultimedia
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Before
 import org.junit.Test
-
 /**
  * Test class for SpotifyMultimedia
  */
 class MockSpotifyMultimediaTest {
-
     private var accessToken = "random"       // access token for Spotify API
     private lateinit var spotifyMultimedia: SpotifyMultimedia
-
     @Before
     fun setup() {
         spotifyMultimedia = SpotifyMultimedia()
     }
-
     /**
      * Test that the album cover image URL is returned correctly
      */
     @Test
     fun getAlbumUriTest() {
-
         val responseBody = """
             {
               "images": [
                 {
                   "url": "http://example.com/image.jpg"
                 }
-                 }
               ]
             }
             """.trimIndent()
-
         val mediaType = "application/json".toMediaTypeOrNull()
         val body = responseBody.toResponseBody(mediaType)
-
         val testResponse = Response.Builder()
             .code(200)
             .message("OK")
@@ -47,24 +38,15 @@ class MockSpotifyMultimediaTest {
             .protocol(Protocol.HTTP_1_1)
             .body(body)
             .build()
-
-
         val spotifyMultimedia = SpotifyMultimedia()
-        val albumUri = spotifyMultimedia.getAlbumCoverImageUrl(
-            "6QPkyl04rXwTGlGlcYaRoW",
-            accessToken,
-            testResponse
-        )
+        val albumUri = spotifyMultimedia.getAlbumCoverImageUrl("6QPkyl04rXwTGlGlcYaRoW" , accessToken, testResponse)
         assert(albumUri.get() == "http://example.com/image.jpg")
-
     }
-
     /**
      * Test that the artist image URL is returned correctly
      */
     @Test
     fun getArtistImageTest() {
-
         val responseBody = """
             {
               "images": [
@@ -74,10 +56,8 @@ class MockSpotifyMultimediaTest {
               ]
             }
             """.trimIndent()
-
         val mediaType = "application/json".toMediaTypeOrNull()
         val body = responseBody.toResponseBody(mediaType)
-
         val testResponse = Response.Builder()
             .code(200)
             .message("OK")
@@ -85,11 +65,17 @@ class MockSpotifyMultimediaTest {
             .protocol(Protocol.HTTP_1_1)
             .body(body)
             .build()
-
         val spotifyMultimedia = SpotifyMultimedia()
         val artistImage = spotifyMultimedia.getArtistImage("0TnOYISbd1XYRBk9myaseg" , accessToken, testResponse)
         assert(artistImage.get() == "http://example.com/image.jpg")
     }
 }
+
+
+
+
+
+
+
 
 

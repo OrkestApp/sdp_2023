d7293b3087090def91b4015d0277e4923d9158e1
Emile Hreich <emile.jd.hreich@gmail.com>
2023-05-26 23:31:08 +0200

    tests

--- app/src/androidTest/java/com/github/orkest/ShareSongTest.kt
+++ app/src/androidTest/java/com/github/orkest/ShareSongTest.kt
@@ -4,11 +4,27 @@ import com.github.orkest.domain.Authorization
 import org.junit.Test
 import android.content.Intent
 import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.github.orkest.data.Constants
+import com.github.orkest.ui.feed.CreatePost
+import com.github.orkest.ui.sharing.Share
 import com.github.orkest.ui.sharing.SharingComposeActivity
 import org.junit.After
 import org.junit.Before
@@ -74,6 +90,10 @@ class SpotifyAuthorizationTest {
         activityRule.scenario.moveToState(Lifecycle.State.DESTROYED)
 
     }
+
+
+
+
 
 
 
